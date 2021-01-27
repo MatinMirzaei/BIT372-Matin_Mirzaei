10a630f76819651e4570894ac2da2a880c598019
Matin Mirzaei <45024401+MatinMirzaei@users.noreply.github.com>
2021-01-26 18:09:03 -0800

    created EventListViewModel class

--- app/src/main/java/com/example/eventlistmvvm/EventListViewModel.java
+++ app/src/main/java/com/example/eventlistmvvm/EventListViewModel.java
@@ -1,8 +1,17 @@
 package com.example.eventlistmvvm;
 
+import android.content.Context;
+
 import androidx.lifecycle.ViewModel;
 
+import javax.net.ssl.SSLSession;
+
 public class EventListViewModel extends ViewModel {
+    public void init(Context applicationContext) {
+    }
+
+    public SSLSession getEvents() {
+    }
     // TODO: Complete the VM class
     // Make the MutableLiveData a singleton
 }
