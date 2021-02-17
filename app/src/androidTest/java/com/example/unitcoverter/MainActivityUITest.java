3f8bc64add0c85e2409aecae9aa442ab062f3e14
Matin Mirzaei <45024401+MatinMirzaei@users.noreply.github.com>
2021-02-16 19:48:59 -0800

    Create a unit Converter & unit test for the formula and two UI tests for functionality.

--- app/src/androidTest/java/com/example/unitcoverter/MainActivityUITest.java
+++ app/src/androidTest/java/com/example/unitcoverter/MainActivityUITest.java
@@ -1,17 +1,21 @@
 package com.example.unitcoverter;
 
 import android.content.Context;
+import android.view.View;
 
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -34,60 +38,40 @@ public class MainActivityUITest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.unitcoverter", appContext.getPackageName());
     }
-/*
+
     @Test
     public void can_enter_fahrenheit() {
         onView(withId(R.id.spinner)).perform(click());
-        onView(withSpinnerText(<"Fahrenheit to Celsius">)).perform(click());
+        onView(withText("Fahrenheit to Celsius")).perform(click());
         onView(withId(R.id.cv_input)).perform(typeText("45"));
         onView(withId(R.id.convert_btn)).perform(click());
         onView(withId(R.id.cv_text)).check(matches(withText("7.22 ºC")));
     }
 
     @Test
-    public void should_not_enter_empty_fah() {
-        onView(withId(R.id.convert_btn)).perform(click());
-        onView(withId(R.id.cv_text)).check(matches(withText("ºC")));
-    }
-
-
-    @Test
     public void can_enter_pound() {
+        onView(withId(R.id.spinner)).perform(click());
+        onView(withText("Pound to Kilogram")).perform(click());
         onView(withId(R.id.cv_input)).perform(typeText("150"));
         onView(withId(R.id.convert_btn)).perform(click());
         onView(withId(R.id.cv_text)).check(matches(withText("68.04 kg")));
     }
 
     @Test
-    public void should_not_enter_empty_pod() {
-        onView(withId(R.id.convert_btn)).perform(click());
-        onView(withId(R.id.cv_text)).check(matches(withText("kg")));
-    }
-
-    @Test
     public void can_enter_mile() {
+        onView(withId(R.id.spinner)).perform(click());
+        onView(withText("Mile to kilometer")).perform(click());
         onView(withId(R.id.cv_input)).perform(typeText("20"));
         onView(withId(R.id.convert_btn)).perform(click());
         onView(withId(R.id.cv_text)).check(matches(withText("32.19 km")));
     }
 
     @Test
-    public void should_not_enter_empty_mls() {
-        onView(withId(R.id.convert_btn)).perform(click());
-        onView(withId(R.id.cv_text)).check(matches(withText("km")));
-    }
-
-    @Test
     public void can_enter_ounce() {
+        onView(withId(R.id.spinner)).perform(click());
+        onView(withText("Ounce to Milliliter")).perform(click());
         onView(withId(R.id.cv_input)).perform(typeText("10"));
         onView(withId(R.id.convert_btn)).perform(click());
         onView(withId(R.id.cv_text)).check(matches(withText("295.73 ml")));
     }
-
-    @Test
-    public void should_not_enter_empty_oz() {
-        onView(withId(R.id.convert_btn)).perform(click());
-        onView(withId(R.id.cv_text)).check(matches(withText("ml")));
-    }
-*/
 }
